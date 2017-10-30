a5ef953d09d65cc778299f894e780a6de63a7658
homebethe e-commerce private limited <homebethe@homebethes-MacBook-Pro.local>
2017-10-30 13:03:14 +0530

    first commit

---
+++ FlyingCart/CartCell.h
@@ -0,0 +1,13 @@
+//
+//  CartCell.h
+//  FlyingCart
+//
+//  Created by homebethe e-commerce private limited on 10/30/17.
+//  Copyright © 2017 homebethe e-commerce private limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CartCell : UITableViewCell
+
+@end
---
+++ FlyingCart/CartCell.m
@@ -0,0 +1,24 @@
+//
+//  CartCell.m
+//  FlyingCart
+//
+//  Created by homebethe e-commerce private limited on 10/30/17.
+//  Copyright © 2017 homebethe e-commerce private limited. All rights reserved.
+//
+
+#import "CartCell.h"
+
+@implementation CartCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
